80909445f33c6a6d6184d7f9b540c3d6dc4e60ce
ascutariu <andreiscutariu92@gmail.com>
2020-01-19 12:09:02 +0200

    updates

---
+++ webapi.consume/wcf.consumer/SoapServiceConsumer/SoapServiceConsumer/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SoapServiceConsumer
+{
+    using System.Threading.Tasks;
+    using WcfService;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceClient = new ServiceClient();
+
+            RunAsync(serviceClient).Wait();
+        }
+
+        private static async Task RunAsync(ServiceClient serviceClient)
+        {
+            var r = await serviceClient.GetDataAsync(20);
+
+            Console.WriteLine(r);
+
+            var r2 = await serviceClient.GetDataUsingDataContractAsync(new CompositeType
+            {
+                BoolValue = true,
+                StringValue = "some"
+            });
+
+            Console.WriteLine(r2.StringValue);
+        }
+    }
+}
