3ddf56f0fc24ea08b1c2df05dfd826052268c5eb
sctrandrei <sctr.andrei@gmail.com>
2020-07-09 12:50:34 +0300

    updated

--- group2/webapi.2/hotelsapi/Hotels.Api/Controllers/RoomsController.cs
+++ group2/webapi.2/hotelsapi/Hotels.Api/Controllers/RoomsController.cs
@@ -6,11 +6,14 @@
     using System.Threading;
     using System.Threading.Tasks;
     using Data;
+    using Data.Entities;
     using Extensions.Map;
     using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Caching.Distributed;
     using Models.Rooms;
+    using Newtonsoft.Json;
     using Services;
 
     [Route("api/hotels/{hotelId}/rooms")]
@@ -20,22 +23,43 @@
     {
         private readonly ApiDbContext context;
         private readonly ISimpleLogger logger;
+        private readonly IDistributedCache distributedCache;
 
-        public RoomsController(ApiDbContext context, ISimpleLogger logger)
+        public RoomsController(ApiDbContext context, ISimpleLogger logger, IDistributedCache distributedCache)
         {
             this.context = context;
             this.logger = logger;
+            this.distributedCache = distributedCache;
         }
 
         [HttpGet("")]
         public async Task<IEnumerable<RoomResource>> Get(int hotelId, CancellationToken token)
         {
-            var list = await this.context.Rooms
-                .Include(h => h.Hotel)
-                .Where(h => h.Hotel.Id == hotelId)
-                .ToListAsync(token);
+            var key = $"_rooms_{hotelId}";
 
-            return list.Select(r => r.MapAsResource());
+            var existingCache = await this.distributedCache.GetStringAsync(key, token: token);
+
+            if (existingCache != null)
+            {
+                var list = JsonConvert.DeserializeObject<List<RoomResource>>(existingCache);
+
+                return list;
+            }
+            else
+            {
+                List<Room> list = await this.context.Rooms
+                    .Include(h => h.Hotel)
+                    .Where(h => h.Hotel.Id == hotelId)
+                    .ToListAsync(token);
+
+                IList<RoomResource> roomResources = list.Select(r => r.MapAsResource()).ToList();
+
+                this.distributedCache.SetString(key, JsonConvert.SerializeObject(roomResources), new DistributedCacheEntryOptions { SlidingExpiration = TimeSpan.FromMinutes(10) });
+
+                this.logger.LogInfo("RoomsController-Get(hotelId) hit");
+
+                return roomResources;
+            }
         }
 
         [HttpGet("{id}")]
