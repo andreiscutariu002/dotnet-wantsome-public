44799ab7b9740a2186fa7e1382222e34ec5bf560
sctrandrei <sctr.andrei@gmail.com>
2020-07-09 16:09:17 +0300

    updates

--- webapi.consume.gr2/HotelApiClient/WebApplicationClient/Startup.cs
+++ webapi.consume.gr2/HotelApiClient/WebApplicationClient/Startup.cs
@@ -1,13 +1,18 @@
 namespace WebApplicationClient
 {
     using System;
+    using System.Net.Http;
     using System.Net.Http.Headers;
+    using System.Threading;
+    using System.Threading.Tasks;
     using Controllers;
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Hosting;
+    using Microsoft.Extensions.Logging;
+    using Polly;
 
     public class Startup
     {
@@ -28,10 +33,17 @@ namespace WebApplicationClient
                 c.BaseAddress = new Uri("http://localhost:5005/");
                 c.DefaultRequestHeaders.Accept.Clear();
                 c.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            });
+            })
+                .AddHttpMessageHandler<LoggingDelegatingHandler>()
+                .AddTransientHttpErrorPolicy(builder => builder.WaitAndRetryAsync(new[]
+                {
+                    TimeSpan.FromSeconds(1),
+                    TimeSpan.FromSeconds(5),
+                    TimeSpan.FromSeconds(10)
+                })); ;
 
             //services.AddHttpClient<HotelClient>();
-
+            services.AddTransient<LoggingDelegatingHandler>();
             services.AddControllers();
         }
 
@@ -45,6 +57,36 @@ namespace WebApplicationClient
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+        }
+    }
+
+    public class LoggingDelegatingHandler : DelegatingHandler
+    {
+        private readonly ILogger<LoggingDelegatingHandler> logger;
+
+        public LoggingDelegatingHandler(ILogger<LoggingDelegatingHandler> logger)
+        {
+            this.logger = logger;
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(
+            HttpRequestMessage request,
+            CancellationToken cancellationToken)
+        {
+            this.logger.LogInformation($"XXX Request: {request}");
+
+            try
+            {
+                // base.SendAsync calls the inner handler
+                var response = await base.SendAsync(request, cancellationToken);
+                this.logger.LogInformation($"XXX Response: {response}");
+                return response;
+            }
+            catch (Exception ex)
+            {
+                this.logger.LogInformation($"XXX Failed to get response: {ex}");
+                throw;
+            }
         }
     }
 }
